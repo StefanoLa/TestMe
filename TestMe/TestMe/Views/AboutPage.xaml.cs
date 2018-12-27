cc3294a8ae086dca19b221e768e84ba9bacb6b74
Stefano Lanfranconi <stlan@hotmail.it>
2018-12-27 19:08:20 +0100

    Prima prova per testare un App per Android sviluppata con Visual Studio

---
+++ TestMe/TestMe/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestMe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
